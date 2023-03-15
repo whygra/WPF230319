5633e07ed61a2e37b2f05a108e890754d7310297
YaroslavGrabovsky <YaroslavGrabovsky@LAPTOP-84CJ014K>
2023-03-15 13:11:48 +0300

    Добавьте файлы проекта.

---
+++ WPF230911/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF230319
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // вычислить и вывести среднее арифметическое
+        private void CalcAvgArithmetic(object sender, RoutedEventArgs e)
+        {
+            if (!AreFieldsFilled()) return;
+            double result = Utils.ArithmeticAvg(
+                Utils.ParseDouble(txbNumA), Utils.ParseDouble(txbNumB), Utils.ParseDouble(txbNumC)
+                );
+            txbResult.Text = 
+                $"Среднее арифметическое: {result:n2}";
+        }
+
+        // вычислить и вывести среднее геометрическое
+        private void CalcAvgGeometric(object sender, RoutedEventArgs e)
+        {
+            // if (!AreFieldsFilled()) return;
+            double result = Utils.GeometicAvg(
+                Utils.ParseDouble(txbNumA), Utils.ParseDouble(txbNumB), Utils.ParseDouble(txbNumC)
+                );
+            txbResult.Text = 
+                $"Среднее геометрическое: {result:n2}";
+        }
+
+        // вычислить корни уравнения
+        private void CalcEquationRoots(object sender, RoutedEventArgs e)
+        {
+            if (!AreFieldsFilled()) return;
+
+            (double a, double b, double c) =
+                (Utils.ParseDouble(txbNumA), Utils.ParseDouble(txbNumB), Utils.ParseDouble(txbNumC));
+
+            double d = Utils.CalcDiscriminant(a, b, c);
+            // квадратный корень из дискриминанта - для упрощенного вычисления корней
+            double sqrtD = Math.Sqrt(d);
+
+            // варианты вычисления и вывода корней в зависимости от дискриминанта
+            txbResult.Text = $"Уравнение ({a}x^2 + {b}x + {c} = 0)\n\n" + 
+                d switch
+            {
+                < 0 => "У уравнения нет корней",
+                > 0 => $"x1 = {Utils.CalcEquationRoot(a, b, sqrtD):n2}, x2 = {Utils.CalcEquationRoot(a, b, -sqrtD):n2}",
+                0 => $"x = {Utils.CalcEquationRoot(a, b, sqrtD):n2}",
+                _ => "",
+            };
+        }
+
+        // проверка вводимых символов
+        private void txbNum_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = Utils.AreCharsAllowed(e.Text);
+        }
+
+        // обработка изменения текста текстбокса для числа
+        private void txbNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // сбросить блок вывода результата (уже не актуального)
+            txbResult.Text = "";
+
+            var txb = (TextBox)sender;
+            // активировать кнопки при успешном парсинге
+            bool isParsed = 
+                btnCalcAvgArithmetic.IsEnabled = btnCalcAvgGeometric.IsEnabled = btnCalcEquationRoots.IsEnabled =
+                Double.TryParse(txb.Text, out double num);
+
+            // передать этому же текстбоксу полученное значение
+            // исключаются возможные лишние символы
+            if (isParsed)
+                // обход пропадания точки
+                txb.Text = Regex.IsMatch(txb.Text, @"\d*\.$") ?
+                    $"{num}." :
+                    $"{num}";
+        }
+
+        // проверка полей на заполненность
+        private bool AreFieldsFilled() =>
+            !String.IsNullOrEmpty(txbNumA.Text) &&
+            !String.IsNullOrEmpty(txbNumB.Text) &&
+            !String.IsNullOrEmpty(txbNumC.Text);
+
+    }
+}
---
+++ WPF230911/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF230319
+{
+    public static class Utils
+    {
+        // регулярное выражение для чисел
+        public static readonly Regex _regexNumChars = new Regex("[^0-9.-]+");
+
+        // проверка строки на соответствие регулярному выражению
+        public static bool AreCharsAllowed(string text) =>
+            _regexNumChars.IsMatch(text);
+
+        public static double ArithmeticAvg(params double[] numbers) =>
+            numbers.Average();
+
+        public static double GeometicAvg(params double[] numbers) =>
+            // корень степени <количество чисел> из произведения всех чисел
+            Root(numbers.Aggregate((a, b) => a * b), numbers.Length);
+
+
+        // извлекает корень степени y из числа x
+        public static double Root(double x, double y) =>
+            Math.Pow(x, 1d / y);
+
+
+        // парсит вещественное число из текстбокса
+        public static double ParseDouble(TextBox txb) =>
+            Double.Parse(txb.Text);
+
+        // дискриминант
+        public static double CalcDiscriminant(double a, double b, double c) =>
+            b * b - 4 * a * c;
+
+        // вычислить корень, входные параметры: коэфициенты a, b и корень из дискриминанта
+        public static double CalcEquationRoot(double a, double b, double sqrtD) =>
+            (-b + sqrtD) / (2 * a);
+    }
+}
